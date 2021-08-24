588294d00e9ca637d8b6d586c11081e1756d23c4
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-08-23 19:25:10 -0700

    Extract out DiseaseCounters panel into its own component.

---
+++ src/Board/FooterPanel/DiseaseCounters.tsx
@@ -0,0 +1,48 @@
+import { faVirus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
+import { Col } from "reactstrap";
+import { Disease } from "../../Shared/Disease";
+import { DiseaseUtils } from "../../Shared/Disease/DiseaseUtils";
+import { EDiseaseType } from "../../Shared/Enums/DiseaseType";
+import { useStores } from "../../Shared/Stores";
+
+export const DiseaseCounters = observer(() => {
+  const { gameStore } = useStores();
+  const [diseaseStates, setDiseaseStates] = useState<
+    { [key in EDiseaseType]: Disease }
+  >(gameStore.diseaseStates);
+  useEffect(() => {
+    setDiseaseStates(gameStore.diseaseStates);
+  }, [gameStore.cities, gameStore.diseaseStates]);
+  return (
+    <>
+      {Object.keys(diseaseStates).map((diseaseKey) => {
+        const diseaseType = parseInt(diseaseKey) as EDiseaseType;
+        return (
+          <Col
+            style={{ width: 100, height: 100 }}
+            className="position-relative d-flex justify-content-center align-items-center"
+          >
+            <span
+              style={{
+                zIndex: -1,
+                fontSize: 50,
+                opacity: 0.5,
+              }}
+              className={`position-absolute ${DiseaseUtils.getDiseaseColorClassName(
+                diseaseType
+              )}`}
+            >
+              <FontAwesomeIcon icon={faVirus} />
+            </span>
+            <span style={{ fontSize: 30, fontWeight: "bold" }}>
+              {diseaseStates[diseaseType].diseaseCount}
+            </span>
+          </Col>
+        );
+      })}
+    </>
+  );
+});
--- src/Board/FooterPanel/index.tsx
+++ src/Board/FooterPanel/index.tsx
@@ -1,21 +1,8 @@
-import { faVirus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { observer } from "mobx-react-lite";
-import { useEffect, useState } from "react";
 import { Col, Container, Row } from "reactstrap";
-import { Disease } from "../../Shared/Disease";
-import { DiseaseUtils } from "../../Shared/Disease/DiseaseUtils";
-import { EDiseaseType } from "../../Shared/Enums/DiseaseType";
-import { useStores } from "../../Shared/Stores";
+import { DiseaseCounters } from "./DiseaseCounters";
 
 export const FooterPanel = observer(() => {
-  const { gameStore } = useStores();
-  const [diseaseStates, setDiseaseStates] = useState<
-    { [key in EDiseaseType]: Disease }
-  >(gameStore.diseaseStates);
-  useEffect(() => {
-    setDiseaseStates(gameStore.diseaseStates);
-  }, [gameStore.cities, gameStore.diseaseStates]);
   return (
     <div
       className="footer-panel d-flex justify-content-end"
@@ -29,33 +16,10 @@ export const FooterPanel = observer(() => {
       <Container>
         <Row>
           <Col></Col>
+          {/* Disease Counter */}
           <Col className="py-3">
             <Row>
-              {Object.keys(diseaseStates).map((diseaseKey) => {
-                const diseaseType = parseInt(diseaseKey) as EDiseaseType;
-                return (
-                  <Col
-                    style={{ width: 100, height: 100 }}
-                    className="position-relative d-flex justify-content-center align-items-center"
-                  >
-                    <span
-                      style={{
-                        zIndex: -1,
-                        fontSize: 50,
-                        opacity: 0.5,
-                      }}
-                      className={`position-absolute ${DiseaseUtils.getDiseaseColorClassName(
-                        diseaseType
-                      )}`}
-                    >
-                      <FontAwesomeIcon icon={faVirus} />
-                    </span>
-                    <span style={{ fontSize: 30, fontWeight: "bold" }}>
-                      {diseaseStates[diseaseType].diseaseCount}
-                    </span>
-                  </Col>
-                );
-              })}
+              <DiseaseCounters />
             </Row>
           </Col>
           <Col></Col>
